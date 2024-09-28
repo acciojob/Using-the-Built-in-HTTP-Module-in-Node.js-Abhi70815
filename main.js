c03bf363f829b820566d05f5c19e514248bff03b
Abhi70815 <abhishekchurasia45621@gmail.com>
2024-09-28 10:45:15 +0530

    save

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const server= http.createServer((req,res)=>{
     res.writeHead(200, {'Content-Type' : "text/plain"});
     res.end("Hello, World! \n");
 });
-
+console.log('Hello, World!');
 // TODO: Listen on port 3000
 const PORT = 3000;
 server.listen(PORT,()=>{
