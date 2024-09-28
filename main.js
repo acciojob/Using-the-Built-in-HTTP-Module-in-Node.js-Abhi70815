52065698baa43e1cc694b4a5e27d40eaa8170ea0
Abhi70815 <abhishekchurasia45621@gmail.com>
2024-09-28 10:40:48 +0530

    save

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const http = require('http');
-
+const fs= require('fs');
 // TODO: Create an HTTP server
+const server= http.createServer((req,res)=>{
+    const filepath = process.argv[2]
+    res.writeHead(200, {'Content-Type' : "text/plain"});
+    res.end("Hello, World! \n");
+});
 
 // TODO: Listen on port 3000
-
+const PORT = 3000;
+server.listen(PORT,()=>{
+    console.log(`Server is liistening on port ${PORT}`);
+});
 // DO NOT EDIT BELOW THIS LINE (Uncomment it once you are done with your code)
 
